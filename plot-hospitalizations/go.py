9265cfc64bf8aabdbf6cc3303fc4305153c28ef8
Alex Couture-Beil <alex@earthly.dev>
2021-07-19 16:00:37 -0700

    plot all together

--- plot-hospitalizations/go.py
+++ plot-hospitalizations/go.py
@@ -9,53 +9,28 @@ vac_hosp_rate = 0.01
 nonvac_hosp_rate = 0.1
 
 vac_rates = []
-num_hospitalization = []
-vac_vs_total_ratio = []
-nonvac_vs_total_ratio = []
 ratio = []
-invratio = []
 
-for vac_rate in frange(0.01, 1.0, 0.01):
+num_vac_hosp_per_1000 = []
+num_nonvac_hosp_per_1000 = []
+
+for vac_rate in frange(0.01, 0.99, 0.01):
     vac_hosp = vac_hosp_rate*vac_rate
     nonvac_hosp = nonvac_hosp_rate*(1.0-vac_rate)
 
+    num_vac_hosp_per_1000.append(vac_hosp*1000)
+    num_nonvac_hosp_per_1000.append(nonvac_hosp*1000)
+
     vac_rates.append(vac_rate)
     total_hospitalizations = vac_hosp+nonvac_hosp
-    num_hospitalization.append(total_hospitalizations*1000.0)
-    vac_vs_total_ratio.append(vac_hosp/total_hospitalizations)
-    nonvac_vs_total_ratio.append(nonvac_hosp/total_hospitalizations)
-    ratio.append(vac_hosp/nonvac_hosp)
+    ratio.append(vac_hosp/total_hospitalizations)
 
-    invratio.append(nonvac_hosp/vac_hosp)
-
-print(vac_rates)
-
-plt.plot(vac_rates, num_hospitalization)
-plt.xlabel('vac rate')
-plt.ylabel('number of hospitalizations per 1000')
-plt.savefig('total-per-1000.png', bbox_inches='tight')
-plt.cla()
-
-plt.plot(vac_rates, vac_vs_total_ratio)
-plt.xlabel('vac rate')
-plt.ylabel('ratio of vaccinated hospitalizations vs total hospitalizations')
-plt.savefig('vac-hospitalizations-vs-total-hospitalizations.png', bbox_inches='tight')
-plt.cla()
-
-plt.plot(vac_rates, nonvac_vs_total_ratio)
-plt.xlabel('vac rate')
-plt.ylabel('ratio of nonvaccinated hospitalizations vs total hospitalizations')
-plt.savefig('nonvac-hospitalizations-vs-total-hospitalizations.png', bbox_inches='tight')
-plt.cla()
-
-plt.plot(vac_rates, ratio)
-plt.xlabel('vac rate')
-plt.ylabel('ratio of vaccinated vs nonvaccinated hospitalizations')
-plt.savefig('vac-hospitalizations-vs-nonvac-hospitalizations.png', bbox_inches='tight')
-plt.cla()
-
-plt.plot(vac_rates, invratio)
-plt.xlabel('vac rate')
-plt.ylabel('ratio of nonvaccinated vs vaccinated hospitalizations')
-plt.savefig('nonvac-hospitalizations-vs-vac-hospitalizations.png', bbox_inches='tight')
+f, sp = plt.subplots(3, sharex=True, sharey=False, figsize=(10.0, 10.0))
+sp[0].plot(vac_rates, num_nonvac_hosp_per_1000)
+sp[0].set_ylabel('number of non vac hosp per 1000')
+sp[1].plot(vac_rates, num_vac_hosp_per_1000)
+sp[1].set_ylabel('number of vac hosp per 1000')
+sp[2].plot(vac_rates, ratio)
+sp[2].set_ylabel('probability hosp patient is vacced')
+plt.savefig('combined.png', bbox_inches='tight')
 plt.cla()
